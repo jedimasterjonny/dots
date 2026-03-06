a0825290e26b36d3c9007fd128b530c70c5c5f2c
jedimasterjonny <46541898+jedimasterjonny@users.noreply.github.com>
2026-03-06 14:49:24 +0000

    Add tressitter plugin to neovim

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
+    build = ":TSUpdate",
+    -- main branch API: install() instead of opts.ensure_installed
+    config = function()
+      require("nvim-treesitter").install({ "lua" })
+    end,
+  },
+}
